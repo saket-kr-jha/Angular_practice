a1f4b1b98dc229c6f1cbf78294dfb89eac6ab674
saket-kr-jha <65586198+saket-kr-jha@users.noreply.github.com>
2024-06-14 23:01:34 +0530

    adding few components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'about', component: AboutUsComponent},
+    {path: 'contact', component: ContactUsComponent},
+];
